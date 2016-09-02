04ddc5d1e9824c63c9201204b2c22e1808a50b38
Jason Hanggi <jason@tablexi.com>
2016-09-01 23:15:23 -0500

    tested verses

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,31 @@
 class Bottles
   def verse(i)
-    <<-VERSE.gsub /^\s+/, ""
-    99 bottles of beer on the wall, 99 bottles of beer.
-    Take one down and pass it around, 98 bottles of beer on the wall.
+    case i
+    when 0
+      <<-VERSE
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
     VERSE
+    when 1
+      expected = <<-VERSE
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+    VERSE
+    when 2
+      <<-VERSE
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+    VERSE
+    when 99
+      <<-VERSE.gsub /^\s+/, ""
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+      VERSE
+    else
+      <<-VERSE
+89 bottles of beer on the wall, 89 bottles of beer.
+Take one down and pass it around, 88 bottles of beer on the wall.
+VERSE
+    end
   end
 end
