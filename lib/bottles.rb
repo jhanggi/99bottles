4480ab9410b9f75e0d6c99366c0f967bae5188ce
Jason Hanggi <jason@tablexi.com>
2016-09-02 00:29:16 -0500

    Introduce BottleCount

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,25 +1,45 @@
+class BottleCount
+  def initialize(count)
+    @count = count
+  end
+
+  def to_i
+    @count
+  end
+
+  def to_s
+    if to_i == 1
+      "#{to_i} bottle"
+    else
+      "#{to_i} bottles"
+    end
+  end
+
+  def -(other)
+    self.class.new(to_i - other.to_i)
+  end
+end
+
 class Bottles
+  TOTAL = 99
+
   def verse(i)
+    count = BottleCount.new(i)
     case i
     when 0
       <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
+Go to the store and buy some more, #{BottleCount.new(TOTAL)} of beer on the wall.
     VERSE
     when 1
       expected = <<-VERSE
-1 bottle of beer on the wall, 1 bottle of beer.
+#{count} of beer on the wall, #{count} of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
-    VERSE
-    when 2
-      <<-VERSE
-2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
     VERSE
     else
       <<-VERSE
-#{i} bottles of beer on the wall, #{i} bottles of beer.
-Take one down and pass it around, #{i-1} bottles of beer on the wall.
+#{count} of beer on the wall, #{count} of beer.
+Take one down and pass it around, #{count - 1} of beer on the wall.
 VERSE
     end
   end
@@ -29,6 +49,6 @@ VERSE
   end
 
   def song
-    verses(99, 0)
+    verses(TOTAL, 0)
   end
 end
