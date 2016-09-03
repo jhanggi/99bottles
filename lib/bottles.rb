ef835d13ae87d9b83e5e390dac74d3660bf41114
Jason Hanggi <jason@tablexi.com>
2016-09-02 22:20:10 -0500

    Remove 1 case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,10 +8,20 @@ class Container
   end
 
   def to_s
-    if to_i == 1
+    if to_i == 0
+      "no more bottles"
+    elsif to_i == 1
       "#{to_i} bottle"
     else
       "#{to_i} bottles"
+    end
+  end
+
+  def pronoun
+    if to_i > 1
+      "one"
+    else
+      "it"
     end
   end
 
@@ -31,15 +41,10 @@ class Bottles
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, #{Container.new(TOTAL)} of beer on the wall.
     VERSE
-    when 1
-      expected = <<-VERSE
-#{count} of beer on the wall, #{count} of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-    VERSE
     else
       <<-VERSE
 #{count} of beer on the wall, #{count} of beer.
-Take one down and pass it around, #{count - 1} of beer on the wall.
+Take #{count.pronoun} down and pass it around, #{count - 1} of beer on the wall.
 VERSE
     end
   end
